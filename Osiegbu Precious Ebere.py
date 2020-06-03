2a0ac219087ab2a9cafe48ad6277815c153b0eba
Osiegbu Precious Ebere <Docphresh91@gmail.com>
2020-06-03 13:25:09 +0100

    Create Osiegbu Precious Ebere.py
    created a  python script

---
+++ Osiegbu Precious Ebere.py
@@ -0,0 +1,6 @@
+#This is my python script for the stage 2 code.
+
+name= "Osiegbu Precious Ebere"
+code="HNG-06360"
+
+print("Hello World, This is", name,"with HNGi7 ID", code, "using Python for stage 2 task")
